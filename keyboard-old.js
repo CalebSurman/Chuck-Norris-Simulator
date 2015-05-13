18e478775fdb288a57d03767dd2406b3f24b6b7d
Zach <kf.manik@gmail.com>
2015-05-13 20:31:29 +1000

    Fixed keyboard errors, some collision issues for you to look at

--- keyboard-old.js
+++ keyboard-old.js
@@ -3,14 +3,13 @@ var Keyboard = function() {
 	var self = this;
 	
 	window.addEventListener('keydown', function(evt) { self.onKeyDown(evt); }, false);
-	window.addEventListener('keyup', function(evt) { self.onKeyUp(evt); }, false);
+	window.addEventListener('keyup', function(evt) {self.onKeyUp(evt); }, false);
 	
 	this.keyListeners = new Array();
-	
 	this.keys = new Array();
 	
-	// key constants
-	// Go here for a list of key codes: https://developer.mozilla.org/en-US/docs/DOM/KeyboardEvent
+	// Key constants. Go here for a list of key codes:
+	// https://developer.mozilla.org/en-US/docs/DOM/KeyboardEvent
 	this.KEY_SPACE = 32;
 	this.KEY_LEFT = 37;
 	this.KEY_UP = 38;
@@ -24,14 +23,14 @@ var Keyboard = function() {
 	this.KEY_SHIFT = 16;
 };
 
-Keyboard.prototype.onKeyDown = function(evt) 
+Keyboard.prototype.onKeyDown = function(evt)
 {
-	this.keys[evt.keyCode] = true;
+	this.keys[evt.keycode] = true;
 };
 
-Keyboard.prototype.onKeyUp = function(evt) 
+Keyboard.prototype.onKeyUp = function(evt)
 {
-	this.keys[evt.keyCode] = false;
+this.keys[evt.keyCode] = false;
 };
 
 Keyboard.prototype.isKeyDown = function(keyCode)
